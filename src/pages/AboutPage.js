976ffb10c9ad8c32d59dfc1994ee352fdaab1a96
Himanshu Gupta <thinktank.himanshu@gmail.com>
2021-02-04 22:04:51 +0530

    basic pages setup

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,11 +1,33 @@
-import React from 'react'
-import styled from 'styled-components'
-import { PageHero } from '../components'
-import aboutImg from '../assets/hero-bcg.jpeg'
+import React from 'react';
+import styled from 'styled-components';
+import { PageHero } from '../components';
+import aboutImg from '../assets/hero-bcg.jpeg';
 
 const AboutPage = () => {
-  return <h4>about page</h4>
-}
+  return (
+    <main>
+      <PageHero title="about" />
+      <Wrapper className="page section section-center">
+        <img src={aboutImg} alt="desk image" />
+        <article>
+          <div className="title">
+            <h2>our story</h2>
+            <div className="underline"></div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. At
+              mollitia illum hic qui, quos tenetur, delectus error reiciendis
+              eius repellendus obcaecati exercitationem minima deserunt
+              repudiandae officiis voluptates possimus nisi aliquid animi
+              eligendi iste fugit? Aut dolore accusantium totam ea repellendus
+              soluta eius ratione? Aut atque voluptas pariatur, laboriosam totam
+              placeat.
+            </p>
+          </div>
+        </article>
+      </Wrapper>
+    </main>
+  );
+};
 
 const Wrapper = styled.section`
   display: grid;
@@ -33,5 +55,5 @@ const Wrapper = styled.section`
   @media (min-width: 992px) {
     grid-template-columns: 1fr 1fr;
   }
-`
-export default AboutPage
+`;
+export default AboutPage;
